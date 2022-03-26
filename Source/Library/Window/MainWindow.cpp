b0f735428e0985774b36ffb9865dc3a9c23fc115
mmindoong <70145314+mmindoong@users.noreply.github.com>
2022-03-26 18:38:34 +0900

    [LAB02] lab02 submitted

---
+++ Source/Library/Window/MainWindow.cpp
@@ -0,0 +1,88 @@
+#include "Window/MainWindow.h"
+
+namespace library
+{
+    /*M+M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M
+      Method:   MainWindow::Initialize
+
+      Summary:  Initializes main window
+
+      Args:     HINSTANCE hInstance
+                  Handle to the instance
+                INT nCmdShow
+                    Is a flag that says whether the main application window
+                    will be minimized, maximized, or shown normally
+                PCWSTR pszWindowName
+                    The window name
+
+      Returns:  HRESULT
+                  Status code
+    M---M---M---M---M---M---M---M---M---M---M---M---M---M---M---M---M-M*/
+
+    HRESULT MainWindow::Initialize(_In_ HINSTANCE hInstance, _In_ INT nCmdShow, _In_ PCWSTR pszWindowName)
+    {
+        RECT rc = { 0, 0, 800, 600 };
+        AdjustWindowRect(&rc, WS_OVERLAPPEDWINDOW, FALSE);
+
+        if (FAILED(initialize(hInstance, nCmdShow, pszWindowName, WS_OVERLAPPED | WS_CAPTION | WS_SYSMENU | WS_MINIMIZEBOX,
+            CW_USEDEFAULT, CW_USEDEFAULT,
+            rc.right - rc.left, rc.bottom - rc.top,
+            nullptr, nullptr)))
+        {
+            return 0;
+        }
+
+        ShowWindow(GetWindow(), nCmdShow);
+
+    }
+
+    /*M+M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M
+      Method:   MainWindow::GetWindowClassName
+
+      Summary:  Returns the name of the window class
+
+      Returns:  PCWSTR
+                  Name of the window class
+    M---M---M---M---M---M---M---M---M---M---M---M---M---M---M---M---M-M*/
+
+    PCWSTR MainWindow::GetWindowClassName() const { return L"TutorialClass"; }
+
+    /*M+M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M
+      Method:   MainWindow::HandleMessage
+
+      Summary:  Handles the messages
+
+      Args:     UINT uMessage
+                  Message code
+                WPARAM wParam
+                    Additional data the pertains to the message
+                LPARAM lParam
+                    Additional data the pertains to the message
+
+      Returns:  LRESULT
+                  Integer value that your program returns to Windows
+    M---M---M---M---M---M---M---M---M---M---M---M---M---M---M---M---M-M*/
+
+    LRESULT MainWindow::HandleMessage(_In_ UINT uMsg, _In_ WPARAM wParam, _In_ LPARAM lParam)
+    {
+        switch (uMsg)
+        {
+        case WM_DESTROY:
+            PostQuitMessage(0);
+            return 0;
+
+        case WM_PAINT:
+        {
+            PAINTSTRUCT ps;
+            HDC hdc = BeginPaint(m_hWnd, &ps);
+            FillRect(hdc, &ps.rcPaint, (HBRUSH)(COLOR_WINDOW + 1));
+            EndPaint(m_hWnd, &ps);
+        }
+        return 0;
+
+        default:
+            return DefWindowProc(m_hWnd, uMsg, wParam, lParam);
+        }
+        return TRUE;
+    }
+}
