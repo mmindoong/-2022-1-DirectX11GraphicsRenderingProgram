fecd8698340b33f3a1e58efb82a6fd5719c755e8
mmindoong <70145314+mmindoong@users.noreply.github.com>
2022-04-12 21:31:49 +0900

    [LAB04] lab04 submitted.

--- Source/Library/Shader/VertexShader.cpp
+++ Source/Library/Shader/VertexShader.cpp
@@ -24,6 +24,7 @@ namespace library
         m_vertexShader = nullptr;
     }
 
+
     /*M+M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M
       Method:   VertexShader::Initialize
 
